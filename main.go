dce07438c51deb0e2d9c896863bf31534746e94f
hirofumi omi <omihirofumi@gmail.com>
2023-07-19 23:20:34 +0900

    add jaeger

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"go.opentelemetry.io/otel/exporters/jaeger"
 	"log"
 
 	"go.opentelemetry.io/otel"
@@ -9,7 +10,10 @@ import (
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
-const serviceName = "anonymous"
+const (
+	jaegerEndpoint = "http://localhost:14268/api/traces"
+	serviceName    = "anonymous"
+)
 
 func createAndRegisterExporters() error {
 	stdExporter, err := stdouttrace.New(
@@ -19,8 +23,16 @@ func createAndRegisterExporters() error {
 		return err
 	}
 
+	jaegerExporter, err := jaeger.New(
+		jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(jaegerEndpoint)),
+	)
+	if err != nil {
+		return err
+	}
+
 	tp := sdktrace.NewTracerProvider(
 		sdktrace.WithSyncer(stdExporter),
+		sdktrace.WithSyncer(jaegerExporter),
 	)
 
 	otel.SetTracerProvider(tp)
