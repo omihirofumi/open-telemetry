1424c81159629817d21218daf6fd1aacf1b28300
hirofumi omi <omihirofumi@gmail.com>
2023-07-19 22:54:23 +0900

    add Foo function

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
-package open_telemetry
+package main
 
 import (
+	"context"
+	"log"
+
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
-	"log"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
-import sdktrace "go.opentelemetry.io/otel/sdk/trace"
+
+const serviceName = "anonymous"
 
 func createAndRegisterExporters() error {
 	stdExporter, err := stdouttrace.New(
@@ -29,4 +33,17 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	tr := otel.GetTracerProvider().Tracer(serviceName)
+
+	ctx, sp := tr.Start(context.Background(), "main")
+	defer sp.End()
+
+	Foo(ctx)
+}
+
+func Foo(ctx context.Context) {
+	tr := otel.GetTracerProvider().Tracer(serviceName)
+	_, sp := tr.Start(ctx, "Foo")
+	defer sp.End()
 }
